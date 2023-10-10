fa8da1629e83fa11973a9c988402a487b356ba09
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-10-10 21:51:44 +0000

    adds comments to the following files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//make sure express is install
 const express = require('express');
-
+//create a express app
 const app = express();
+//set the port
+const port = 3000;
 
-// Serve static files from the `static` folder
+//serve static files(eg, .html .js etc) from the folder static for the code of you express app 
 app.use(express.static('static'));
 
 // Start the server
-var port = "3000"
 app.listen(port);
 console.log('app listening on port ' + port + '')
