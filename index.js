0bf6d8e1403396cbfa4f142d87f7bbba3a053d34
I don't codddddde <120886940+asc2563@users.noreply.github.com>
2023-10-13 01:56:15 +0000

    update: added console log

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //make sure express is install
-const express = require('express');
+const express = require("express");
 //create a express app
 const app = express();
 //set the port
 const port = 3000;
 
-//serve static files(eg, .html .js etc) from the folder static for the code of you express app 
-app.use(express.static('static'));
+//serve static files(eg, .html .js etc) from the folder static for the code of you express app
+app.use(express.static("static"));
 
 // Start the server
-app.listen(port);
-console.log('app listening on port ' + port + '')
+app.listen(port),
+  () => {
+    console.log(`app listening on localhost:${port}`);
+  };
 // End of the server code for Aluben go to static folder for front-end code
