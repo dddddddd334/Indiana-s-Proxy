c1e276c04d1a0576c23e31a51008dced0097871d
asc2563 <allistersaintclair@outlook.com>
2023-11-15 14:13:50 -0800

    modified index.js in .

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-const express = require("express"); // Import Express
-const path = require("path"); // Import path module
-const port = process.env.PORT || 3000; // Set server port
+const http = require("http");
+const path = require("path");
+const port = process.env.PORT || 3000;
 
-const app = express(); // Create an Express app
-
-// Serve static files from the 'public' directory
-app.use(express.static("public"));
-
-// Route for '/' serving the index.html file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const app = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  } else if (req.url === "/credits") {
+    res.sendFile(path.join(__dirname, "public", "credits.html"));
+  } else if (req.url === "/byeblock") {
+    res.sendFile(path.join(__dirname, "public", "Byeblocker.html"));
+  } else if (req.url === "/fart") {
+    res.sendFile(path.join(__dirname, "public", "Fart.html"));
+  } else if (req.url === "/list") {
+    res.sendFile(path.join(__dirname, "public", "list.html"));
+  } else {
+    res.sendFile(path.join(__dirname, "public", "404.html"));
+  }
 });
 
-// Route for '/credits' serving the credits.html file
-app.get("/credits", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "credits.html"));
-});
-
-// Route for '/byeblock' serving the Byeblocker.html file
-app.get("/byeblock", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Byeblocker.html"));
-});
-
-// Route for '/fart' serving the Fart.html file
-app.get("/fart", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Fart.html"));
-});
-
-// Route for '/list' serving the list.html file
-app.get("/list", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "list.html"));
-});
-
-// Handle 404 errors
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log(`app listening on port ${port} at localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
